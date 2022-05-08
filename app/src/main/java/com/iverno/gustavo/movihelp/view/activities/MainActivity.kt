149c5362eefc276e2f04639669c9f25299410700
Gustavo Londa <gustavolonda@gmail.com>
2022-05-08 03:23:09 -0500

    [New][Add Filter]

---
+++ app/src/main/java/com/iverno/gustavo/movihelp/view/activities/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.iverno.gustavo.movihelp.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.StrictMode
+import com.iverno.gustavo.movihelp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+    }
+}
